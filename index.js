0d303dcaeaa1f436fe2128341fc85c8cc3e57cd9
Philip Powers <philip_powers607@hotmail.com>
2021-10-02 15:39:08 -0400

    employee information inputted into bootstrap cards

--- index.js
+++ index.js
@@ -1,101 +1,118 @@
 const inquirer = require("inquirer");
 const pageTemplate = require("./src/page-template.js");
-const {generateManager, generateEngineer, generateIntern} = require("./src/card");
-const fs = require('fs');
+const {
+  generateManager,
+  generateEngineer,
+  generateIntern,
+} = require("./src/card.js");
+const fs = require("fs");
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
 const employeeCards = [];
 
-
 const manager = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Who is the manager?",
-      //manager.name input to cards
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is their employee ID number?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is their email address?",
-    },
-    {
-      type: "input",
-      name: "office",
-      message: "What is their office number?",
-    },
-  
-  ])
-  .then(managerData => {
-   var managerCard =  generateManager(managerData);
-   employeeCards.push(managerCard);
-  })
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Who is the manager?",
+        //manager.name input to cards
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is their employee ID number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is their email address?",
+      },
+      {
+        type: "input",
+        name: "office",
+        message: "What is their office number?",
+      },
+    ])
+    .then((managerData) => {
+      const managerCard = generateManager(managerData);
+      employeeCards.push(managerCard);
+      console.log(managerCard);
+    });
 };
 
-const promptEmployee = teamData => {
-  if (!teamData.employees) {
-      teamData.employees = [];
-  }
+const promptEmployee = () => {
   console.log(`
         ===============
         Add an Employee
         ===============
     `);
-  return inquirer.prompt([
-    {
-      type: "list",
-      name: "employeeType",
-      message: "Is this person an Engineer or an Intern?",
-      choices: ["Engineer", "Intern"],
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the name of this employee?'
-    },
-    {
-      type: 'number',
-      name: 'id',
-      message: 'What is their employee id number?'
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is their email address?'
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'What is their GitHub account name?',
-      when: (input) => input.employeeType === "Engineer"
-    },
-    {
-      type: 'input',
-      name: 'school',
-      message: 'What school are they attending?',
-      when: (input) => input.employeeType === "Intern"
-    },
-    {
-      type: 'confirm',
-      name: 'confirmAddEmployee',
-      message: 'Would you like to add another employee?',
-      default: false
-    }
-  ])
-  .then(employeeData => {
-    console.log(employeeData);
-    teamData.employees.push(employeeData);
-    if (employeeData.confirmAddEmployee) {
-      return promptEmployee(teamData);
-    } else {
-      return teamData;
-    }
-  })
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "Is this person an Engineer or an Intern?",
+        choices: ["Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of this employee?",
+      },
+      {
+        type: "number",
+        name: "id",
+        message: "What is their employee id number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is their email address?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is their GitHub account name?",
+        when: (input) => input.role === "Engineer",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school are they attending?",
+        when: (input) => input.role === "Intern",
+      },
+      {
+        type: "confirm",
+        name: "confirmAddEmployee",
+        message: "Would you like to add another employee?",
+        default: false,
+      },
+    ])
+    .then((teamData) => {
+      const { name, id, email, role, github, school, confirmAddEmployee } =
+        teamData;
 
+      if (role === "Engineer") {
+        engineerData = new Engineer(name, id, email, github);
+        engineerCard = generateEngineer(engineerData);
+        employeeCards.push(engineerData);
+        console.log(engineerCard);
+      } else if (role === "Intern") {
+        internData = new Intern(name, id, email, school);
+        internCard = generateIntern(internData);
+        employeeCards.push(internData);
+        console.log(internCard);
+      }
+
+      if (confirmAddEmployee) {
+        return promptEmployee(employeeCards);
+      } else {
+        return employeeCards;
+      }
+    })
 };
 
 manager()
--- src/card.js
+++ src/card.js
@@ -1,35 +1,51 @@
 function generateManager(Manager) {
     return `
-    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-<div class="card-header text-center">${Manager.name}</div>
-<div class="card-body">
-  <h5 class="card-title">Manager</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-</div>
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${Manager.name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted"><i class="fas fa-chalkboard-teacher"></i>Manager</h6>
+    <p class="card-text">
+        Employee ID: ${Manager.id}
+        <br>
+        Employee Office Number: ${Manager.office}
+    </p>
+    <a href="mailto:${Manager.email}" class="card-link">${Manager.email}</a>
+  </div>
 </div>
     `
 }
 
 function generateEngineer(engineer) {
     return `
-    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-<div class="card-header text-center">${engineer.name}</div>
-<div class="card-body">
-  <h5 class="card-title">Engineer</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-</div>
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${engineer.name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted"><i class="fas fa-laptop-code"></i></i>Engineer</h6>
+    <p class="card-text">
+        Employee ID: ${engineer.id}
+        <br>
+        Employee GitHub Account:<a href="https://github.com/${engineer.github}">${engineer.github}</a>
+        <br>
+        Email:<a href="mailto:${engineer.email}" class="card-link">${engineer.email}</a>
+    </p>
+  </div>
 </div>
     `
 }
 
 function generateIntern(intern) {
     return `
-    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-<div class="card-header text-center">${intern.name}</div>
-<div class="card-body">
-  <h5 class="card-title">Intern</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-</div>
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${intern.name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted"><i class="fas fa-graduation-cap"></i>Intern</h6>
+    <p class="card-text">
+        Employee ID: ${intern.id}
+        <br>
+        School Attending: ${intern.school}
+    </p>
+    <a href="mailto:${intern.email}" class="card-link">${intern.email}</a>
+  </div>
 </div>
     `
 }
